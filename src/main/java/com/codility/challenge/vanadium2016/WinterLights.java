52d3b60b787a14c06083e63d982a1794cca07ee7
Nickolay Mazurkin <mazurkin@gmail.com>
2016-12-09 19:56:11 +0300

    WinterLights

--- src/main/java/com/codility/challenge/vanadium2016/WinterLights.java
+++ src/main/java/com/codility/challenge/vanadium2016/WinterLights.java
@@ -5,6 +5,105 @@ package com.codility.challenge.vanadium2016;
  */
 public class WinterLights {
 
+    public int solution(String s) {
+        return solutionBest(s, 10);
+    }
+
+    public int solutionBest(String s, int dictionarySize) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        final int n = s.length();
+        final char[] chars = s.toCharArray();
+
+        // Space is 2^dictionarySize when each symbol need 1 bit (odd or even)
+        final int capacity = 1 << dictionarySize;
+
+        // Index of array is bitmask "b10/b9/b8/b7/b6/b5/b4/b3/b2/b1" where
+        //    bX == 1 means that X occurred odd times
+        //    bX == 0 means that X occurred even times
+        final int[] counts = new int[capacity];
+
+        int globalCount = 0;
+        int globalMask = 0;
+
+        for (int i = 0; i < n; i++) {
+            final char c = chars[i];
+            final int localMask = bitmask(c);
+
+            // Non-modified `globalMask` equals to current 0-index
+            counts[globalMask]++;
+
+            // Remap internal addressing instead of array copying
+            globalMask = globalMask ^ localMask;
+
+            // The only good variants are (with history correction):
+            // - when no set bits (all symbol counts are even)
+            // - when only 1 bit is set (only 1 symbol count is odd)
+            // Modified `globalMask` equals to modified 0-index
+            int localCount = counts[globalMask];
+            for (int j = 1; j < capacity; j = j << 1) {
+                localCount += counts[j ^ globalMask];
+            }
+
+            globalCount += localCount;
+        }
+
+        return globalCount;
+    }
+
+
+    public int solutionGood(String s, int dictionarySize) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        final int n = s.length();
+        final char[] chars = s.toCharArray();
+
+        // Space is 2^dictionarySize when each symbol need 1 bit (odd or even)
+        final int capacity = 1 << dictionarySize;
+
+        // Index of array is bitmask "b10/b9/b8/b7/b6/b5/b4/b3/b2/b1" where
+        //    bX == 1 means that X occurred odd times
+        //    bX == 0 means that X occurred even times
+        int[] counts1 = new int[capacity];
+        int[] counts2 = new int[capacity];
+
+        int globalCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            final char c = chars[i];
+            final int mask = bitmask(c);
+
+            // Transferring counts for this mask (excessive copying is eliminated in the `best` method):
+            // - from odd to even
+            // - from even to odd
+            for (int j = 0; j < capacity; j++) {
+                counts2[j ^ mask] = counts1[j];
+            }
+
+            counts2[mask]++;
+
+            // The only good variants are:
+            // - when no set bits (all symbol counts are even)
+            // - when only 1 bit is set (only 1 symbol count is odd)
+            int localCount = counts2[0];
+            for (int j = 1; j < capacity; j = j << 1) {
+                localCount += counts2[j];
+            }
+
+            globalCount += localCount;
+
+            final int[] tmp = counts1;
+            counts1 = counts2;
+            counts2 = tmp;
+        }
+
+        return globalCount;
+    }
+
     public int solutionSlow(String s) {
         if (s == null || s.isEmpty()) {
             return 0;
@@ -29,11 +128,6 @@ public class WinterLights {
             }
 
             globalCount = globalCount + localCount;
-
-//            System.out.printf("'%s'%n", s.substring(0, i + 1));
-//            System.out.printf("step=%3d; local=%3d; loss=%3d; total=%3d%n",
-//                    i + 1, localCount, i + 1 - localCount, globalCount);
-//            System.out.println();
         }
 
         return globalCount;
@@ -42,7 +136,5 @@ public class WinterLights {
     private static int bitmask(char c) {
         return 1 << (c - '0');
     }
-
-
 
 }
--- src/test/java/com/codility/challenge/vanadium2016/WinterLightsTest.java
+++ src/test/java/com/codility/challenge/vanadium2016/WinterLightsTest.java
@@ -14,19 +14,43 @@ public class WinterLightsTest {
     }
 
     @Test
-    public void testSlow1() throws Exception {
+    public void testBest() throws Exception {
+        Assert.assertEquals(0, solution.solutionBest("", 10));
+        Assert.assertEquals(1, solution.solutionBest("0", 10));
+        Assert.assertEquals(2, solution.solutionBest("01", 10));
+
+        Assert.assertEquals(41, solution.solutionBest("1011100011", 10));
+
+        Assert.assertEquals(11, solution.solutionBest("02002", 10));
+
+        Assert.assertEquals(10, solution.solutionBest("0123456789", 10));
+        Assert.assertEquals(23, solution.solutionBest("01234567890123456789", 10));
+    }
+
+    @Test
+    public void testGood() throws Exception {
+        Assert.assertEquals(0, solution.solutionGood("", 10));
+        Assert.assertEquals(1, solution.solutionGood("0", 10));
+        Assert.assertEquals(2, solution.solutionGood("01", 10));
+
+        Assert.assertEquals(41, solution.solutionGood("1011100011", 10));
+
+        Assert.assertEquals(11, solution.solutionGood("02002", 10));
+
+        Assert.assertEquals(10, solution.solutionGood("0123456789", 10));
+        Assert.assertEquals(23, solution.solutionGood("01234567890123456789", 10));
+    }
+
+    @Test
+    public void testSlow() throws Exception {
         Assert.assertEquals(0, solution.solutionSlow(""));
         Assert.assertEquals(1, solution.solutionSlow("0"));
-        Assert.assertEquals(10, solution.solutionSlow("0123456789"));
-    }
 
-    @Test
-    public void testSlow2() throws Exception {
         Assert.assertEquals(41, solution.solutionSlow("1011100011"));
-    }
 
-    @Test
-    public void testSlow3() throws Exception {
         Assert.assertEquals(11, solution.solutionSlow("02002"));
+
+        Assert.assertEquals(10, solution.solutionSlow("0123456789"));
+        Assert.assertEquals(23, solution.solutionSlow("01234567890123456789"));
     }
 }
