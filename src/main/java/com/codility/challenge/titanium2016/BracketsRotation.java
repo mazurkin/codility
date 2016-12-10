496954f3c797a8c1cda2330869b4122fa4e84811
Nickolay Mazurkin <mazurkin@gmail.com>
2016-12-10 14:28:49 +0300

    brackets

--- src/main/java/com/codility/challenge/titanium2016/BracketsRotation.java
+++ src/main/java/com/codility/challenge/titanium2016/BracketsRotation.java
@@ -5,7 +5,62 @@ package com.codility.challenge.titanium2016;
  */
 public class BracketsRotation {
 
-    public int solution(final String s, final int k) {
+    public int solution(String s, int k) {
+        return solutionSlow(s, k);
+    }
+
+    public int solutionSlow(String s, int k) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        final int n = s.length();
+        if (k >= n / 2 + 1) {
+            return n;
+        }
+
+        final char[] chars = s.toCharArray();
+
+        int globalResult = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int balance = 0;
+            int result = 0;
+            int budget = k;
+
+            for (int j = i, rest = n - i; j < n; j++, rest--) {
+                if (chars[j] == '(') {
+                    if (balance + 1 <= rest - 1) {
+                        balance++;
+                    } else if (budget > 0) {
+                        balance--;
+                        budget--;
+                    } else {
+                        break;
+                    }
+                } else {
+                    if (balance > 0) {
+                        balance--;
+                    } else if (budget > 0) {
+                        balance++;
+                        budget--;
+                    } else {
+                        break;
+                    }
+                }
+
+                if (balance == 0) {
+                    result = Math.max(result, j - i + 1);
+                }
+            }
+
+            globalResult = Math.max(globalResult, result);
+        }
+
+        return globalResult;
+    }
+
+    public int solutionFast(final String s, final int k) {
         if (s == null || s.isEmpty()) {
             return 0;
         }
@@ -18,23 +73,23 @@ public class BracketsRotation {
 
         final char[] brackets = s.toCharArray();
 
-        return solution(brackets, 0, N, k);
+        return solutionFast(brackets, 0, N, k);
     }
 
-    private int solution(final char[] brackets, final int offset, final int count, final int k) {
+    private int solutionFast(final char[] brackets, final int offset, final int count, final int k) {
         if (count < 2) {
             return 0;
         }
 
         if (count % 2 == 1) {
-            final int r1 = solution(brackets, offset, count - 1, k);
-            final int r2 = solution(brackets, offset + 1, count - 1, k);
+            final int r1 = solutionFast(brackets, offset, count - 1, k);
+            final int r2 = solutionFast(brackets, offset + 1, count - 1, k);
             return Math.max(r1, r2);
         }
 
         int r1 = 0;
 
-        final int r2 = solution(brackets, offset + 1, count - 1, k);
+        final int r2 = solutionFast(brackets, offset + 1, count - 1, k);
 
         return Math.max(r1, r2);
     }
